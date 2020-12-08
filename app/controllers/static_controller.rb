4aca3456782b2bcd1fb1747ef0864420fcc57604
Rahel_Hagos <hagos.rahelh@gmail.com>
2020-12-07 19:56:13 -0500

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,11 @@
+class StaticController < ApplicationController
+
+  def hellow_world
+    render "hellow_world"
+  end 
+
+  def about
+
+  end 
+
+  end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'hellow_world', to: 'static#hellow_world'
+  get 'about', to: 'static#about'
 end
